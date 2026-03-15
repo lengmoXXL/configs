c50cf4d7f9cd60aa120c2d158d7b1b1aec399953
lengmoXXL <forlearn_lzy@163.com>
2026-03-15 09:38:11 +0800

    nvim: change diffview keymaps from <leader>g to <leader>j

--- configs/nvim/lua/plugins/diffview.lua
+++ configs/nvim/lua/plugins/diffview.lua
@@ -3,9 +3,9 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
   cmd = { 'DiffviewOpen', 'DiffviewClose', 'DiffviewFileHistory' },
   keys = {
-    { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview window" },
-    { "<leader>gh", "<cmd>DiffviewOpen HEAD^<cr>", desc = "Open Diffview window" },
-    { "<leader>gH", "<cmd>DiffviewFileHistory<cr>", desc = "Open Diffview window" },
-    { "<leader>gc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview window" },
+    { "<leader>jd", "<cmd>DiffviewOpen<cr>", desc = "Open Diffview window" },
+    { "<leader>jh", "<cmd>DiffviewOpen HEAD^<cr>", desc = "Open Diffview window" },
+    { "<leader>jH", "<cmd>DiffviewFileHistory<cr>", desc = "Open Diffview window" },
+    { "<leader>jc", "<cmd>DiffviewClose<cr>", desc = "Close Diffview window" },
   },
 }
