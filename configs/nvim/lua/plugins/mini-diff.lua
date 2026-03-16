d251f2790f526968db0eee6fd3eae13821296ef2
lengmoXXL <forlearn_lzy@163.com>
2026-03-16 20:21:18 +0800

    nvim: add diff overlay toggle keymap
    - Set mini.diff to load immediately (lazy = false)
    - Add <leader>to to toggle diff overlay

--- configs/nvim/lua/plugins/mini-diff.lua
+++ configs/nvim/lua/plugins/mini-diff.lua
@@ -1,5 +1,9 @@
 return {
   'nvim-mini/mini.diff',
   version = '*',
+  lazy = false,
   opts = {},
+  keys = {
+    { '<leader>to', function() require('mini.diff').toggle_overlay() end, desc = 'Toggle diff overlay' },
+  }
 }
