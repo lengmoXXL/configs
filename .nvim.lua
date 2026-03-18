c0e6e26725982d7539458732c1a68473d6c78b0c
lengmoXXL <forlearn_lzy@163.com>
2026-03-18 22:33:23 +0800

    add .nvim.lua for lua_ls project config

---
+++ .nvim.lua
@@ -0,0 +1,12 @@
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      runtime = { version = 'LuaJIT' },
+      diagnostics = { globals = { 'vim' } },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+    },
+  },
+})
+vim.lsp.enable('lua_ls')
