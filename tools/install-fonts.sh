6e4957fa74d71d28d66af23b5ee6dde5dfbd11d5
lengmoXXL <forlearn_lzy@163.com>
2026-04-01 23:35:41 +0800

    tools: install-fonts.sh 兼容 macOS bash 3.x

--- tools/install-fonts.sh
+++ tools/install-fonts.sh
@@ -7,24 +7,41 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 FONTS_DIR="$SCRIPT_DIR/../fonts"
 PROXY="${GITHUB_PROXY:-https://gh-proxy.com/}"
 
-# 字体列表
-declare -A FONTS=(
-    ["sarasa"]="Sarasa Term SC Nerd|${PROXY}https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v2.3.1/SarasaTermSCNerd.ttf.tar.gz|tar.gz"
-    ["aurulent"]="AurulentSansMono Nerd Font|${PROXY}https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/AurulentSansMono.zip|zip"
-)
+get_font_info() {
+    case "$1" in
+        sarasa)
+            echo "Sarasa Term SC Nerd|${PROXY}https://github.com/laishulu/Sarasa-Term-SC-Nerd/releases/download/v2.3.1/SarasaTermSCNerd.ttf.tar.gz|tar.gz"
+            ;;
+        aurulent)
+            echo "AurulentSansMono Nerd Font|${PROXY}https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/AurulentSansMono.zip|zip"
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
+list_fonts() {
+    echo "可用字体:"
+    echo "  sarasa   - Sarasa Term SC Nerd"
+    echo "  aurulent - AurulentSansMono Nerd Font"
+}
 
 download_font() {
     local name="$1"
-    local info="${FONTS[$name]}"
+    local info
+    info=$(get_font_info "$name")
 
-    if [[ -z "$info" ]]; then
+    if [[ $? -ne 0 ]]; then
         echo "错误: 未知字体 '$name'"
-        echo "可用字体: ${!FONTS[*]}"
+        list_fonts
         return 1
     fi
 
     local display_name url format
-    IFS='|' read -r display_name url format <<< "$info"
+    display_name=$(echo "$info" | cut -d'|' -f1)
+    url=$(echo "$info" | cut -d'|' -f2)
+    format=$(echo "$info" | cut -d'|' -f3)
 
     echo "下载 $display_name ..."
 
@@ -49,11 +66,7 @@ download_font() {
 usage() {
     echo "用法: $0 [字体名...]"
     echo ""
-    echo "可用字体:"
-    for name in "${!FONTS[@]}"; do
-        IFS='|' read -r display_name _ _ <<< "${FONTS[$name]}"
-        echo "  $name  - $display_name"
-    done
+    list_fonts
     echo ""
     echo "不带参数则安装全部字体"
 }
@@ -65,9 +78,8 @@ trap "rm -rf $TMPDIR" EXIT
 if [[ $# -eq 0 ]]; then
     # 安装全部
     echo "安装全部字体..."
-    for name in "${!FONTS[@]}"; do
-        download_font "$name"
-    done
+    download_font "sarasa"
+    download_font "aurulent"
 elif [[ "$1" == "-h" || "$1" == "--help" ]]; then
     usage
 else
