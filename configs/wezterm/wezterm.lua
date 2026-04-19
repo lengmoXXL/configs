b864076c2c5dab09b9417f4c4a104d32b8a34415
lengmoXXL <forlearn_lzy@163.com>
2026-04-19 10:19:31 +0800

    Update wezterm theme selector

--- configs/wezterm/wezterm.lua
+++ configs/wezterm/wezterm.lua
@@ -3,7 +3,37 @@ local act = wezterm.action
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'GitHub Dark'
+config.color_scheme = 'Vs Code Dark+ (Gogh)'
+
+local function scheme_choices()
+    local schemes = wezterm.color.get_builtin_schemes()
+    local choices = {}
+
+    for name, _ in pairs(schemes) do
+        table.insert(choices, { label = name, id = name })
+    end
+
+    table.sort(choices, function(a, b)
+        return a.label < b.label
+    end)
+
+    return choices
+end
+
+local select_color_scheme = act.InputSelector {
+    title = 'Select color scheme',
+    fuzzy = true,
+    choices = scheme_choices(),
+    action = wezterm.action_callback(function(window, pane, id, label)
+        if not id then
+            return
+        end
+
+        local overrides = window:get_config_overrides() or {}
+        overrides.color_scheme = id
+        window:set_config_overrides(overrides)
+    end),
+}
 
 config.font = wezterm.font_with_fallback {
     'AurulentSansM Nerd Font',
@@ -42,6 +72,11 @@ config.keys = {
         action = act.PaneSelect {
             mode = 'SwapWithActive',
         },
+    },
+    {
+        key = 'T',
+        mods = 'CTRL|SHIFT',
+        action = select_color_scheme,
     },
 }
 
