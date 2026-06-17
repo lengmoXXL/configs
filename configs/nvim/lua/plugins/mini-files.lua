ae7467ad8d6490f04b4830d222acd930b2bd1211
lengmoXXL <forlearn_lzy@163.com>
2026-06-17 21:46:37 +0800

    Scope mini.files keys to normal buffers

--- configs/nvim/lua/plugins/mini-files.lua
+++ configs/nvim/lua/plugins/mini-files.lua
@@ -2,6 +2,35 @@ return {
   'nvim-mini/mini.files',
   version = '*',
   cmd = { 'MiniFilesToggleSort' },
+  init = function()
+    local group = vim.api.nvim_create_augroup('MiniFilesBufferKeymaps', { clear = true })
+    local set_buffer_keymap = function(bufnr)
+      if vim.bo[bufnr].buftype ~= '' or not vim.bo[bufnr].buflisted then
+        return
+      end
+
+      vim.keymap.set('n', '<leader>e', function()
+        local path = vim.api.nvim_buf_get_name(0)
+        local mini_files = require('mini.files')
+        if not mini_files.close() then
+          mini_files.open(path ~= '' and path or vim.fn.getcwd())
+          mini_files.reveal_cwd()
+        end
+      end, { buffer = bufnr, desc = 'Toggle mini.files (current file location)' })
+
+      vim.keymap.set('n', '<leader>E', function()
+        require('mini.files').open(vim.fn.getcwd())
+      end, { buffer = bufnr, desc = 'Open mini.files (cwd)' })
+    end
+
+    set_buffer_keymap(vim.api.nvim_get_current_buf())
+    vim.api.nvim_create_autocmd('BufEnter', {
+      group = group,
+      callback = function(args)
+        set_buffer_keymap(args.buf)
+      end,
+    })
+  end,
   opts = {
     mappings = {
       go_in = '<C-l>',
@@ -62,24 +91,4 @@ return {
       vim.notify(('mini.files sort: %s'):format(next_mode == sort_ascending and 'name ascending' or 'name descending'))
     end, { desc = 'Toggle mini.files filename sort order', force = true })
   end,
-  keys = {
-    {
-      "<leader>e",
-      function()
-        local mini_files = require('mini.files')
-        if not mini_files.close() then
-          mini_files.open(vim.api.nvim_buf_get_name(0))
-          mini_files.reveal_cwd()
-        end
-      end,
-      desc = "Toggle mini.files (current file location)",
-    },
-    {
-      "<leader>E",
-      function()
-        require('mini.files').open(vim.fn.getcwd())
-      end,
-      desc = "Open mini.files (cwd)",
-    },
-  },
 }
